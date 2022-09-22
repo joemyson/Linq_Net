3267cff7fdba007a231a1b235037dcafd54b93ef
joemyson <joemyson1402@gmail.com>
2022-09-22 02:46:47 -0300

    busca simple com Contains

--- Consulta_Linq/Program.cs
+++ Consulta_Linq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Consulta_Linq
 {
@@ -6,7 +8,46 @@ namespace Consulta_Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // sem usa o linq
+            var generos = new List<Genero>
+           {
+               new Genero{Id=01,Nome="rock"},
+               new Genero{Id=02,Nome="Forro"},
+               new Genero{Id=03,Nome ="Rock classico"},
+               new Genero{Id=04,Nome ="Samba"}
+
+           };
+
+            foreach (var genero in generos)
+            {
+                if (genero.Nome.Contains("Rock"))
+                {
+                    Console.WriteLine("{0}\t{1}", genero.Id, genero.Nome);
+                }
+            }
+
+            Console.WriteLine();
+
+
+            // usando linq
+
+            var query = from g in generos
+                        where g.Nome.Contains("Rock")
+                        select g;
+
+            foreach (var g in query)
+            {
+                Console.WriteLine("{0}\t{1}", g.Id, g.Nome);
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
     }
 }
