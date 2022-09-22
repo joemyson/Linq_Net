eae220829aa27406aacf22c087b323d463e4ce34
joemyson <joemyson1402@gmail.com>
2022-09-22 02:14:23 -0300

    iniciado projeto

---
+++ Consulta_Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Consulta_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
